f6891d64b831ca01b50f6a3fa63d8b4eef36c529
BrantDesu <chamorrobrant@gmail.com>
2023-04-06 21:21:05 -0400

    feat: first commit

---
+++ src/main/java/com/nttlab/springboot/BackendEntregaG3Application.java
@@ -0,0 +1,13 @@
+package com.nttlab.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEntregaG3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEntregaG3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttlab/springboot/BackendEntregaG3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttlab.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEntregaG3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
