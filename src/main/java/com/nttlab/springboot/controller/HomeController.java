4c3655dfd4a88bd8a0d338ae6d2c4d3e64f0dadc
Walker1718 <cibanepa@SAN-50W9MQ2.usersad.everis.int>
2023-04-07 00:21:57 -0400

    commit porfavor

---
+++ src/main/java/com/nttlab/springboot/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.nttlab.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping(value= {"","/","/home","/index"})
+	public String home(Model model) {
+		model.addAttribute("titulo","Springboot + JPA");
+		return "home";
+	}
+
+}
