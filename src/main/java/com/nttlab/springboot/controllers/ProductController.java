9cde2abc342731d2e43e8e7c2f4785fcee2b29e4
BrantDesu <chamorrobrant@gmail.com>
2023-04-09 20:15:01 -0400

    feat: product files (service, interfaces, etc)

--- src/main/java/com/nttlab/springboot/controllers/ProductController.java
+++ src/main/java/com/nttlab/springboot/controllers/ProductController.java
@@ -1,16 +1,106 @@
 package com.nttlab.springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.nttlab.springboot.models.entity.Product;
+import com.nttlab.springboot.models.service.iProductService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
 	
-	@GetMapping(value= "productList")
+	@Autowired
+	private iProductService productService;
+	
+	@GetMapping(value= "/product/list")
 	public String ProductList(Model model) {
 		model.addAttribute("title","Listado de Productos");
-		return "productList";
+		return "listProduct";
+	}
+	
+	@GetMapping(value = "/product/new")
+	public String createProduct(Model model) {
+		Product product = new Product();
+		model.addAttribute("producto", product);
+		model.addAttribute("title", "Formulario Creación producto");
+		return "formProduct";
+	}
+	
+	@PostMapping(value = "/product/form")
+	public String saveProduct(@Valid Product product, BindingResult result, Model model, RedirectAttributes flash,
+			SessionStatus status) {
+		String mensajeFlash = null;		
+		if(result.hasErrors()) {
+			model.addAttribute("title","Formulario Creación producto");
+			return "formProduct";
+		}
+		
+		
+		if(product.getIdProduct() != null) {
+			productService.save(product);
+			return "redirect:listProduct";			
+		}
+		else {
+			if(productService.findByName(product.getName()) != null ) {
+				flash.addFlashAttribute("error", "Rut ya registrado en nuestro sistema");
+				return "redirect:formProduct";
+			}
+			productService.save(product);
+			status.setComplete();
+			flash.addFlashAttribute("success", mensajeFlash);
+			return "redirect:listProduct";
+		}
+	}
+	
+	@GetMapping(value="/product/edit/{id_product}")
+	public String editProduct(@PathVariable(value="id_user") Long id_user, Model model, RedirectAttributes flash) {
+		Product product = null;
+		if(id_user > 0) {
+			product = productService.findOne(id_user);
+			if(product == null) {
+				flash.addFlashAttribute("clase", "danger");
+				flash.addFlashAttribute("error", "El producto buscado no se encuentra en nuestros registros");
+				return "redirect:listProduct";
+			}
+			else {
+				model.addAttribute("producto", product);
+				model.addAttribute("titulo", "Formulario Edición producto");
+				return "formProduct";
+			}
+		}
+		else
+		{
+			flash.addFlashAttribute("clase", "danger");
+			flash.addFlashAttribute("error", "Debes ingresar un valor mayor a 0");
+			return "redirect:listProduct";
+		}
+	}
+	
+	
+	@GetMapping(value="/delete/{id_user}")
+	public String deleteProduct(@PathVariable(value="id_product") Long id_product, Model model, RedirectAttributes flash) {
+		Product product = productService.findOne(id_product);
+		if(product == null) {
+			flash.addFlashAttribute("clase", "danger");
+			flash.addFlashAttribute("error", "El producto buscado no se encuentra en nuestros registros");
+			return "redirect:listProduct";
+		}
+		else
+		{
+			productService.delete(id_product);
+			flash.addFlashAttribute("clase", "success");
+			flash.addFlashAttribute("success", "Producto eliminado con éxito");
+			return "redirect:listProduct";
+		}
 	}
 
 }
