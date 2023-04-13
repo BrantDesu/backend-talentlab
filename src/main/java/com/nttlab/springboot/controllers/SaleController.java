4fed8445f503187e64097c0d764f8474839a620b
elinksnow <elizabeth.elcl@gmail.com>
2023-04-13 11:52:29 -0400

    feat: download boleta

--- src/main/java/com/nttlab/springboot/controllers/SaleController.java
+++ src/main/java/com/nttlab/springboot/controllers/SaleController.java
@@ -1,66 +1,87 @@
 package com.nttlab.springboot.controllers;
 
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.itextpdf.text.DocumentException;
 import com.nttlab.springboot.models.entity.Cart;
-import com.nttlab.springboot.models.entity.CartItem;
-import com.nttlab.springboot.models.entity.Product;
 import com.nttlab.springboot.models.entity.Sale;
-import com.nttlab.springboot.models.entity.Client;
 import com.nttlab.springboot.models.service.iCartService;
 import com.nttlab.springboot.models.service.iSaleService;
 import com.nttlab.springboot.models.service.iUserService;
 
 import jakarta.validation.Valid;
 
+import com.nttlab.springboot.models.entity.PDFGenerator;
+
 @Controller
 public class SaleController {
 	
 	@Autowired
 	private iSaleService saleService;
-	
+
 	@Autowired
 	private iCartService cartService;
-	
+
 	@Autowired
 	private iUserService userService;
-	
+
 	@PostMapping(value = { "/sale/create" })
-	public String saveSale(@RequestParam Long cart_id, Model model) {
-		Cart cart = cartService.findOne(cart_id);
-        Sale sale = new Sale(cart.getUser(), cart, cart.calculateCartTotal());
-//        List<CartItem> ci = cart.getCart_items();
-//        for (CartItem cartItem : ci) {
-//			System.out.println(cartItem.getTotal() * cartItem.getQuantity());
-//		}
-//        
-//        System.out.println(cart.calculateCartTotal());
-        
-        saleService.save(sale);
-        
-        
-        /* TODO
-         * setear estado del carrito a inactivo
-         * setear un nuevo carrito para el usuario (SIN BORRAR EL ANTERIOR)
-         * Actualizar stock productos
-         * */
-        
-        
-        
-		model.addAttribute("titulo", "Sale");
-		model.addAttribute("sale", sale);
-		return "sale";
+	public String saveSale(@RequestParam Long cart_id, Model model)  {
+	    Cart cart = cartService.findOne(cart_id);
+	    Sale sale = new Sale(cart.getUser(), cart, cart.calculateCartTotal());
+
+	    saleService.save(sale);
+
+	    model.addAttribute("titulo", "Sale");
+	    model.addAttribute("sale", sale);
+	    return "sale";
 	}
+	
+	
+	
+	@GetMapping(value={"/sale/download/{sale_id}"})
+	public ResponseEntity<InputStreamResource> downloadPdf(@PathVariable (name="sale_id") Long sale_id) {
+		System.out.println("llegue");
+		Sale sale = saleService.findOne(sale_id);
+		PDFGenerator pdfGenerator = new PDFGenerator();
+	    return pdfGenerator.generatePDF(sale);
+	    
+	}
+	
+	
+
+	/*@GetMapping(value={"/sale/download"})
+	public ResponseEntity<InputStreamResource> downloadPdf(@RequestParam Long sale_id) throws IOException, FileNotFoundException, DocumentException {
+	    Sale sale = saleService.findOne(sale_id);
+	    PDFGenerator pdfGenerator = new PDFGenerator();
+	    ByteArrayInputStream bis = pdfGenerator.generatePDF(sale);
+
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.add("Content-Disposition", "attachment; filename=boleta.pdf");
+
+	    InputStreamResource isr = new InputStreamResource(bis);
+
+	    return ResponseEntity.ok()
+	            .headers(headers)
+	            .contentType(MediaType.APPLICATION_PDF)
+	            .body(isr);
+	}*/
+
+	
 	
 
 	
